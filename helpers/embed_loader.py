19b810cfb83c17300d27c141b60ede1f375a2097
alvesmarcos <alves.henrique.marcos@gmail.com>
2020-05-26 21:33:07 -0300

    fix: split data for train

--- helpers/embed_loader.py
+++ helpers/embed_loader.py
@@ -2,4 +2,5 @@ from fairseq import options, utils
 
 embed_dict = utils.parse_embedding('generated_embeds_encoder.txt')
 
+print(embed_dict['i'])
 print(len(embed_dict))
--- helpers/split_data.py
+++ helpers/split_data.py
@@ -2,14 +2,15 @@ import argparse
 
 from sklearn.model_selection import train_test_split
 
+
 def eng_prefix(line):
-    prefix = { 
-        "i'm": "i am", 
+    prefix = {
+        "i'm": "i am",
         "you're": "you are",
         "it's": "it is",
         "she's": "she is",
-        "he's": "he is", 
-        "we're": "we are", 
+        "he's": "he is",
+        "we're": "we are",
         "they're": "they are"
     }
     _line = list()
@@ -17,6 +18,7 @@ def eng_prefix(line):
     for w in words:
         _line.append(prefix[w] if w in prefix else w)
     return ' '.join(_line)
+
 
 def read(path):
     src_sentences = list()
@@ -26,37 +28,49 @@ def read(path):
         for line in fd:
             source, target = line.split('\t')
             src_sentences.append(eng_prefix(source))
-            tgt_sentences.append(target[:-1]) # remove \n
+            tgt_sentences.append(target[:-1])  # remove \n
     return src_sentences, tgt_sentences
 
+
 def split(data_src, data_tgt):
-    X_valid, X_test, y_valid, y_test = train_test_split(
-        data_src, data_tgt, test_size=0.5)
-    return (X_valid, X_test), (y_valid, y_test)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data_src, data_tgt, test_size=0.3)
+
+    X_test, X_valid, y_test, y_valid = train_test_split(
+        X_test, y_test, test_size=0.5
+    )
+    return (X_train, X_valid, X_test), (y_train, y_valid, y_test)
+
 
 def write(path, data):
     with open(path, 'w') as fd:
         for d in data:
             fd.write(f'{d}\n')
 
+
 def run(path):
-    source_path = ('data/tatoeba-valid.fr-en.en', 'data/tatoeba-test.fr-en.en')
-    target_path = ('data/tatoeba-valid.fr-en.fr', 'data/tatoeba-test.fr-en.fr')
+    source_path = ('data/tatoeba-train.fr-en.en',
+                   'data/tatoeba-valid.fr-en.en', 'data/tatoeba-test.fr-en.en')
+    target_path = ('data/tatoeba-train.fr-en.fr',
+                   'data/tatoeba-valid.fr-en.fr', 'data/tatoeba-test.fr-en.fr')
     source, target = read(path)
     src_data, tgt_data = split(source, target)
-    
-    # the length must be the same 
+
+    # the length must be the same
     assert len(src_data) == len(tgt_data)
 
     for src_path, tgt_path, src, tgt in zip(source_path, target_path, src_data, tgt_data):
         write(src_path, src)
         write(tgt_path, tgt)
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Helper to split the Tatoeba dataset')
+    parser = argparse.ArgumentParser(
+        description='Helper to split the Tatoeba dataset')
     # add arguments
-    parser.add_argument('path', type=str, help='The path of the Tatoeba English-French dataset')
+    parser.add_argument(
+        'path', type=str, help='The path of the Tatoeba English-French dataset')
     # process args
-    args = parser.parse_args()    
+    args = parser.parse_args()
     # run script
     run(args.path)
