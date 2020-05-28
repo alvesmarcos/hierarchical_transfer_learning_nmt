58156dd48d833032938ade40aa08b43dab87691f
alvesmarcos <alves.henrique.marcos@gmail.com>
2020-05-27 22:05:09 -0300

    feat: add script to test embeds

--- helpers/embed_loader.py
+++ helpers/embed_loader.py
@@ -1,6 +1,22 @@
+import argparse
+
 from fairseq import options, utils
 
-embed_dict = utils.parse_embedding('generated_embeds_encoder.txt')
+def run(path):
+  embed_dict = utils.parse_embedding(path)
+  while True:
+    try:
+      _input = input('Type a word: ')
+      print(embed_dict[_input])
+    except:
+      print('Sorry this word is not in the embed')
 
-print(embed_dict['i'])
-print(len(embed_dict))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Test if the embed was load correctly')
+    # add arguments
+    parser.add_argument('path', type=str, help='The path of the Embed file')
+    # process args
+    args = parser.parse_args()
+    # run script
+    run(args.path)
